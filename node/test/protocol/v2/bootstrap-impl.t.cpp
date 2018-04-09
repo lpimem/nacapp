81848a5d3857b690918ab6588793316241e61e0a
lpimem <lpi@memphis.edu>
2018-04-09 16:56:42 -0500

    sync

---
+++ node/test/protocol/v2/bootstrap-impl.t.cpp
@@ -0,0 +1,25 @@
+
+#include "../../../../shared/src/hmac.hpp"
+
+#include "../../../src/protocol/v2/bootstrap-impl.hpp"
+
+
+namespace nacapp {
+namespace bootstrap {
+
+const std::string prefixText = "/local-home";
+const std::string ownerNameText = prefixText + "/owner";
+const std::string deviceIDText = "Make1-Model2-Version345";
+const std::stirng devicePinText = "uns4fe";
+
+
+TEST_CASE("hmac")
+{
+  const std::string content = "/local-home/bootstrap/owner/Make1-Model2-Version345/1923856";
+  const std::string expect = "c4231e0ae5717a7e8cad7837a78e7f5884b4e0553315f6c6dcad667011e586db";
+  std::string hash = sign_hmac(devicePinText, content);
+  REQUIRE(hash == expect);
+}
+
+} // namespace bootstrap
+} // namespace nacapp
