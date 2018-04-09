ee545c0e95a4e4b3d69443c05ef880713a22553d
lpimem <lpi@memphis.edu>
2018-04-09 16:32:05 -0500

    sync

---
+++ node/src/protocol/v2/bootstrap-impl.hpp
@@ -0,0 +1,32 @@
+#ifndef NODE_BOOTSTRAP_V2_IMPL_HPP
+#define NODE_BOOTSTRAP_V2_IMPL_HPP
+
+#include "../../../../shared/src/common-ndn.hpp"
+
+namespace nacapp {
+namespace bootstrap {
+
+extern bool
+verifyHash(std::string content, std::string key, std::string hash);
+
+extern bool
+validateOwnerCertResp(const Data& d, std::string expected_r, std::string pin);
+
+extern bool
+validateOwnerRequest(const Interest& interest, const Name& args, std::string pin);
+
+extern void
+serveDeviceUnsignedCert(std::shared_ptr<BootstrapHelper> helper,
+                        OnStatusChange onSuccess,
+                        OnStatusChange onFailure);
+
+extern void
+startBootstrap(Name ownerName,
+               std::string deviceID,
+               std::shared_ptr<BootstrapHelper> helper,
+               OnStatusChange onSuccess,
+               OnStatusChange onFailure);
+
+} // namespace bootstrap
+} // namespace nacapp
+#endif
