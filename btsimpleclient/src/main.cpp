a6987d10da89864570e7c37b7fb8a94133ad3341
Luke Pi <lowatt.pi@gmail.com>
2018-11-16 01:19:05 -0600

    update bootstrap example

--- btsimpleclient/src/main.cpp
+++ btsimpleclient/src/main.cpp
@@ -6,6 +6,7 @@
 
 #include "common.hpp"
 #include "../../node/src/node.hpp"
+#include "../../node/src/protocol/v2/bootstrap.hpp"
 
 
 INITIALIZE_EASYLOGGINGPP
@@ -49,9 +50,31 @@ main()
   signal(SIGSEGV, crash_handler);
 
   LOG(INFO) << ">>> START >>>";
-  const std::string PREFIX = "/local-home/NAC";
+
+  const std::string BOOTSTRAP_DEVICE_PREFIX = "/local-home/bootstrap/device";
+  const std::string DEVICE_PAIRING_ID = "SampleDevice12345";
+  const std::string DEVICE_PAIRING_CODE = "Unsafe12345";
+
   auto f = std::make_shared<ndn::Face>("127.0.0.1");
   auto keychain = nacapp::createMemoryKeyChain();
+  auto node = std::make_shared<nacapp::Node>(BOOTSTRAP_DEVICE_PREFIX, f, keychain);
+
+  auto session = std::make_shared<nacapp::bootstrap::Session>();
+  session->keychain = keychain;
+
+  nacapp::bootstrap::Config config;
+  config.pairId = DEVICE_PAIRING_ID;
+  config.pairCode = DEVICE_PAIRING_CODE;
+  config.wellknown = Name("/local-home/bootstrap");
+
+
+  nacapp::bootstrap::start(node,
+                           session,
+                           config,
+                           []() -> { LOG(INFO) << "BOOTSTRAP SUCCESS"; },
+                           [](const std::string& reason) -> {
+                             LOG(ERROR) << "BOOTSTRAP FAILED " << reason;
+                           });
 
   LOG(INFO) << "<<< EXIT <<<";
   return 0;
